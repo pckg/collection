09f7c4ad23125aa5eb76fc929216c8554edbddc1
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-01-22 23:03:39 +0100

    Reimplement Each

--- src/Pckg/Collection/Each.php
+++ src/Pckg/Collection/Each.php
@@ -6,24 +6,55 @@ class Each
 {
 
     /**
-     * @var Collection
+     * @var Collection|Each
      */
     protected $collection;
 
-    public function __construct(Collection $collection)
+    protected $property;
+
+    public function __construct($collection, $property = null)
     {
         $this->collection = $collection;
+        $this->property = $property;
+    }
+
+    public function goDeep(callable $callback)
+    {
+        if ($this->collection instanceof Collection) {
+            return $this->collection->each($callback);
+        }
+
+        $property = $this->property;
+
+        return $this->collection->goDeep(
+            function($item) use ($property, $callback) {
+                return $callback($item->{$property});
+            }
+        );
     }
 
     public function __call($name, $args)
     {
-        $this->collection->each(
-            function($item) use ($name, $args) {
-                call_user_func_array([$item, $name], $args);
+        $this->collection->goDeep(
+            function($collection) use ($name, $args) {
+                $collection->each(
+                    function($item) use ($name, $args) {
+                        call_user_func_array([$item, $name], $args);
+                    }
+                );
             }
         );
 
         return $this->collection;
+    }
+
+    public function __get($name)
+    {
+        if ($name == 'each') {
+            return new Each($this);
+        } else {
+            return (new Each($this, $name));
+        }
     }
 
 }
